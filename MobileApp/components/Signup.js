1523562feb7fd7918a5afef5ba4b8c5f0d7dd40d
Jacob Bermudez <95668622+Jacob607b@users.noreply.github.com>
2023-06-20 14:33:51 -0700

    Update Signup.js

--- MobileApp/components/Signup.js
+++ MobileApp/components/Signup.js
@@ -57,36 +57,18 @@ export default function Signup({ navigation }) {
   const handleID = (idEntered) => {
     set_id(idEntered);
   };
-  //Handle submit to validate the ID entered against the Hased values stored in MongoDB
-  const handleIDSubmit = () => {
-    //Sends AccountID entered to enpoint /Account_ID on the server as a post, body contains the account code
-    const fetchOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ account_ID: id }),
-    };
-    fetch("http://10.0.2.2:5000/Acccount_ID", fetchOptions).then(
-      async (res) => {
-        //If the status of the fetch response is OK, UI is updated with Registration form
-        if (res.status == 200) {
-          const test = await res.json();
-          setID(test.id);
-          alert("Success!");
-        } else {
-          setID(null);
-          alert("Issue with entered Account ID");
-        }
-      }
-    );
-  };
+ 
   const URI = "../assets/mountainBackground.jpg";
   //Initalize Screen variable, to define UI of this page based on state of ID context
   let Screen;
-  //If ID context == true
-  if (ID) {
+ 
     Screen = (
+
+    );
+ 
+  return (
+    <>
+      <ImageBackground source={require(URI)} style={{ height: "100%" }}>
       <View style={styles.container}>
         <TextInput
           style={styles.textInput}
@@ -104,27 +86,6 @@ export default function Signup({ navigation }) {
           <Text style={styles.txt}>Sign up</Text>
         </TouchableOpacity>
       </View>
-    );
-  } else {
-    Screen = (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.textInput}
-          value={id}
-          onChangeText={handleID}
-          placeholder="Enter Account ID"
-        />
-        <TouchableOpacity style={styles.button} onPress={handleIDSubmit}>
-          <Text style={styles.txt}>Submit</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-
-  return (
-    <>
-      <ImageBackground source={require(URI)} style={{ height: "100%" }}>
-        {Screen}
       </ImageBackground>
     </>
   );
