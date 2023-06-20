adef507b460f8f2f235b3234aab68dabec3329e4
Jacob Bermudez <95668622+Jacob607b@users.noreply.github.com>
2023-06-20 14:27:20 -0700

    Update App.js

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -19,10 +19,6 @@ import { IDContext } from "./context/IDContext";
 import { ReadContext } from "./context/ReadContext";
 
 export default function App() {
-  //======= IGNORES ASYNC STORAGE WARING CASUED BY EXPO ==========
-  LogBox.ignoreLogs([
-    "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
-  ]);
 
   const Stack = createNativeStackNavigator();
 
